de9a38c19831b31a05d2a01ab38bc31a4d568f87
gregoryromanov <gregoryromanov@gmail.com>
2021-06-09 13:20:57 +0300

    FIXED

--- 1. array reduce/reduce.js
+++ 1. array reduce/reduce.js
@@ -1,7 +1,7 @@
 Array.prototype.my_reduce = function(f, acc) {
-	if(!!this.length) acc = acc ? acc += this[0] : this[0];
-	for(let i = 1; i < this.length; i++) acc = f(acc, this[i], i, this);
-	return acc;
+  if(!!this.length) acc = acc ? acc += this[0] : this[0];
+  for(let i = 1; i < this.length; i++) acc = f(acc, this[i], i, this);
+  return acc;
 }
 
 console.log([1, 2, 3, 4].my_reduce((acc, n) => acc + n));
